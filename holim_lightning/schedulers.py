21cf2751a14a21179ae367f266d1c0d0189625fe
Holim Lim <holim0711@outlook.com>
2021-03-01 00:40:43 +0900

    fix typo

--- holim_lightning/schedulers.py
+++ holim_lightning/schedulers.py
@@ -3,12 +3,12 @@ import math
 
 
 class CosineAnnealingWarmUp(torch_scheds.LambdaLR):
-    def __init__(self, optimizer, T_max, T_warm=0, eta_min=0, last_epoch=-1):
+    def __init__(self, optimizer, T_max, T_warm=0, last_epoch=-1):
         def lr_lambda(t):
             if t < T_warm:
                 return float(t) / float(T_warm)
-            lrate = float(t - T_warm) / float(T_max - T_warm)
-            return 0.5 * (1 + math.cos(lrate * math.pi))
+            θ = float(t - T_warm) / float(T_max - T_warm)
+            return 0.5 * (1 + math.cos(θ * math.pi))
         super().__init__(optimizer, lr_lambda, last_epoch)
 
 
