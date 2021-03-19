b37b89a6017fa772be2673baecbe9e3bdef348af
Holim Lim <holim0711@outlook.com>
2021-03-19 19:35:32 +0900

    add T_mute

--- holim_lightning/schedulers.py
+++ holim_lightning/schedulers.py
@@ -8,10 +8,12 @@ class ConstantLR(torch.optim.lr_scheduler.LambdaLR):
 
 
 class CosineAnnealingWarmUp(torch.optim.lr_scheduler.LambdaLR):
-    def __init__(self, optimizer, T_max, T_warm=0, last_epoch=-1):
+    def __init__(self, optimizer, T_max, T_warm=0, T_mute=0, last_epoch=-1):
         def lr_lambda(t):
-            if t < T_warm:
-                return float(t) / float(T_warm)
+            if t < T_mute:
+                return 0
+            elif t < T_warm:
+                return (float(t) - float(T_mute)) / (float(T_warm) - float(T_mute))
             elif t < T_max:
                 θ = float(t - T_warm) / float(T_max - T_warm)
                 return 0.5 * (1 + math.cos(θ * math.pi))
