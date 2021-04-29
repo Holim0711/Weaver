cbc168cf2176a65f74e47817cefa2585d6739740
Holim Lim <holim0711@outlook.com>
2021-04-29 18:05:49 +0900

    add lars

--- holim_lightning/optimizers.py
+++ holim_lightning/optimizers.py
@@ -1,8 +1,11 @@
 import torch
+from pl_bolts.optimizers import LARS
 
 
 def get_optim(params, name, **kwargs):
     if name == 'AdaBound':
         from adabound import AdaBound
         return AdaBound(params, **kwargs)
+    elif name == 'LARS':
+        return LARS(params, **kwargs)
     return torch.optim.__dict__[name](params, **kwargs)
