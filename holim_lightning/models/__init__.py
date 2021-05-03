2e50339cb07068b5a1ce23d13db1f25f98c30182
Holim Lim <holim0711@outlook.com>
2021-05-03 17:58:58 +0900

    get_model & get_encoder

--- holim_lightning/models/__init__.py
+++ holim_lightning/models/__init__.py
@@ -1,42 +1,29 @@
-import sys
 import torch
-from torchvision import models as torch_models
-from .custom.wrn28 import build_wide_resnet28
 
 
-def get_efficientnet(backbone, pretrained, **kwargs):
-    try:
+def get_model(src, name, pretrained=False, **kwargs):
+    if src == 'torchvision':
+        from torchvision import models
+        return models.__dict__[name](pretrained=pretrained, **kwargs)
+    elif src == 'lukemelas':
         from efficientnet_pytorch import EfficientNet
-    except:
-        print('Run "pip install efficientnet_pytorch"', file=sys.stderr)
-        raise
-    if pretrained:
-        return EfficientNet.from_pretrained(backbone, **kwargs)
+        if pretrained is True:
+            return EfficientNet.from_pretrained(name, **kwargs)
+        else:
+            return EfficientNet.from_name(name, **kwargs)
+    elif src == 'custom':
+        from .custom import get_custom_model
+        return get_custom_model(name, **kwargs)
     else:
-        return EfficientNet.from_name(backbone, **kwargs)
+        raise ValueError(f"Unknown model source {src}")
 
 
-def get_torch_model(backbone, pretrained, **kwargs):
-    return torch_models.__dict__[backbone](pretrained=pretrained, **kwargs)
-
-
-def get_model(backbone, num_classes, pretrained=True, **kwargs):
-    if backbone.startswith('efficientnet'):
-        model = get_efficientnet(backbone, pretrained, num_classes=num_classes)
-    elif backbone.startswith('wide_resnet28'):
-        assert not pretrained, "only custom models available"
-        return build_wide_resnet28(backbone, num_classes, **kwargs)
-    else:
-        model = get_torch_model(backbone, pretrained)
-        model.fc = torch.nn.Linear(model.fc.in_features, num_classes)
-    return model
-
-
-def get_encoder(backbone, pretrained=True):
-    if backbone.startswith('efficientnet'):
-        model = get_efficientnet(backbone, pretrained, num_classes=num_classes)
+def get_encoder(**kwargs):
+    model = get_model(**kwargs)
+    if hasattr(model, 'fc'):
+        model.fc = torch.nn.Identity()
+    elif hasattr(model, '_fc'):
         model._fc = torch.nn.Identity()
     else:
-        model = get_torch_model(backbone, pretrained)
-        model.fc = torch.nn.Identity()
+        raise NotImplementedError("This model doesn't have 'fc' or '_fc'")
     return model
