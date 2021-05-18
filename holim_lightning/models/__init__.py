c40de499f49206dbdc0a492cfe4fe6925930c773
Holim Lim <holim0711@outlook.com>
2021-05-18 17:21:41 +0900

    return dim info for encoders

--- holim_lightning/models/__init__.py
+++ holim_lightning/models/__init__.py
@@ -21,9 +21,11 @@ def get_model(src, name, pretrained=False, **kwargs):
 def get_encoder(**kwargs):
     model = get_model(**kwargs)
     if hasattr(model, 'fc'):
+        dim = model.fc.in_features
         model.fc = torch.nn.Identity()
     elif hasattr(model, '_fc'):
+        dim = model.fc.in_features
         model._fc = torch.nn.Identity()
     else:
         raise NotImplementedError("This model doesn't have 'fc' or '_fc'")
-    return model
+    return model, dim
