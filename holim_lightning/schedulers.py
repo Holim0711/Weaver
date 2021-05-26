531ba4fb94c8150af7d2dfb2d2ec700d89f4a218
Holim Lim <holim0711@outlook.com>
2021-05-26 23:10:14 +0900

    wow

--- holim_lightning/schedulers.py
+++ holim_lightning/schedulers.py
@@ -1,18 +1,9 @@
-import torch
-
-
-class ConstantLR(torch.optim.lr_scheduler.LambdaLR):
-    def __init__(self, optimizer, last_epoch=-1):
-        super().__init__(optimizer, lambda x: 1, last_epoch)
-
-
 def get_sched(optim, name, **kwargs):
-    if name is None or name == 'ConstantLR':
-        Scheduler = ConstantLR
-    elif name == 'LinearWarmupCosineAnnealingLR':
+    if name == 'LinearWarmupCosineAnnealingLR':
         from pl_bolts.optimizers import LinearWarmupCosineAnnealingLR
         Scheduler = LinearWarmupCosineAnnealingLR
     else:
+        import torch
         Scheduler = torch.optim.lr_scheduler.__dict__[name]
 
     return Scheduler(optim, **kwargs)
