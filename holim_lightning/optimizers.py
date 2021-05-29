3a14cec4855a7d71407e481f1268f0ec8d5d05a3
Holim Lim <holim0711@outlook.com>
2021-05-29 19:21:43 +0900

    add utils for optim

--- holim_lightning/optimizers.py
+++ holim_lightning/optimizers.py
@@ -1,4 +1,7 @@
-def get_optim(param, name, **kwargs):
+import torch
+
+
+def get_optim(module_or_params, name, **kwargs):
     if name == 'AdaBound':
         from adabound import AdaBound
         Optimizer = AdaBound
@@ -6,7 +9,24 @@ def get_optim(param, name, **kwargs):
         from pl_bolts.optimizers import LARS
         Optimizer = LARS
     else:
-        import torch
         Optimizer = torch.optim.__dict__[name]
 
-    return Optimizer(param, **kwargs)
+    if isinstance(module_or_params, torch.nn.Module):
+        module_or_params = module_or_params.parameters()
+
+    return Optimizer(module_or_params, **kwargs)
+
+
+def exclude_wd(module, skip_list=['bias', 'bn']):
+    params = []
+    excluded_params = []
+
+    for name, param in module.named_parameters():
+        if not param.requires_grad:
+            continue
+        elif any(k in name for k in skip_list):
+            excluded_params.append(param)
+        else:
+            params.append(param)
+
+    return [{'params': params}, {'params': excluded_params, 'weight_decay': 0}]
