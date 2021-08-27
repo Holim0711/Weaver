2e0565144e1f7cb951b794f87e16430eca6e7987
Holim Lim <holim0711@outlook.com>
2021-08-27 18:02:20 +0900

    lr_dict + epochs to steps

--- holim_lightning/schedulers.py
+++ holim_lightning/schedulers.py
@@ -7,3 +7,19 @@ def get_sched(optim, name, **kwargs):
         Scheduler = torch.optim.lr_scheduler.__dict__[name]
 
     return Scheduler(optim, **kwargs)
+
+
+def get_lr_dict(optim, scheduler, n_train_iters=None, **lr_dict):
+    scheduler = dict(scheduler)
+    if lr_dict['interval'] == 'step':
+        if scheduler['name'] == 'CosineAnnealingLR':
+            scheduler['T_max'] *= n_train_iters
+        elif scheduler['name'] == 'CosineAnnealingWarmRestarts':
+            scheduler['T_0'] *= n_train_iters
+        elif scheduler['name'] == 'LinearWarmupCosineAnnealingLR':
+            scheduler['warmup_epochs'] *= n_train_iters
+            scheduler['max_epochs'] *= n_train_iters
+        elif scheduler['name'] in {'CyclicLR', 'OneCycleLR'}:
+            raise NotImplementedError("Unsupoorted lr scheduler yet")
+    lr_dict['scheduler'] = get_sched(optim, **scheduler)
+    return lr_dict
