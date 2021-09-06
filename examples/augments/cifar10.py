560efd75029afd45936542ba365674e56fdb311e
Holim Lim <holim0711@outlook.com>
2021-09-06 11:46:45 +0900

    update exmples

--- examples/augments/cifar10.py
+++ examples/augments/cifar10.py
@@ -1,15 +1,23 @@
 import os
-import sys
 import json
+import argparse
+
 import torch
-from torchvision import datasets
+from torchvision.datasets import CIFAR10, CIFAR100
 import torchmetrics
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
+
 from holim_lightning.models import get_model
 from holim_lightning.optimizers import get_optim
-from holim_lightning.schedulers import get_sched
+from holim_lightning.schedulers import get_lr_dict
 from holim_lightning.transforms import get_trfms
+
+
+datasets = {
+    'CIFAR10': CIFAR10,
+    'CIFAR100': CIFAR100,
+}
 
 
 class Module(pl.LightningModule):
@@ -17,37 +25,32 @@ class Module(pl.LightningModule):
     def __init__(self, **kwargs):
         super().__init__()
         self.save_hyperparameters()
-
         self.model = get_model(**self.hparams.model)
         self.criterion = torch.nn.CrossEntropyLoss()
         self.accuracy = torch.nn.ModuleDict({
-            '_train': torchmetrics.Accuracy(),
-            '_val': torchmetrics.Accuracy(),
-            '_test': torchmetrics.Accuracy(),
+            'trn': torchmetrics.Accuracy(),
+            'val': torchmetrics.Accuracy(),
         })
 
     def shared_step(self, phase, batch):
         x, y = batch
         z = self.model(x)
         loss = self.criterion(z, y)
-        self.accuracy['_' + phase].update(z.softmax(dim=-1), y)
+        self.accuracy[phase].update(z.softmax(dim=-1), y)
         return {'loss': loss}
 
     def shared_epoch_end(self, phase, outputs):
         loss = torch.stack([x['loss'] for x in outputs]).mean()
-        acc = self.accuracy['_' + phase].compute()
-        self.log_dict({
-            f'{phase}/loss': loss,
-            f'{phase}/acc': acc,
-            'step': self.current_epoch,
-        })
-        self.accuracy['_' + phase].reset()
+        self.log(f'{phase}/loss', loss)
+        acc = self.accuracy[phase].compute()
+        self.log(f'{phase}/acc', acc)
+        self.accuracy[phase].reset()
 
     def training_step(self, batch, batch_idx):
-        return self.shared_step('train', batch)
+        return self.shared_step('trn', batch)
 
     def training_epoch_end(self, outputs):
-        self.shared_epoch_end('train', outputs)
+        self.shared_epoch_end('trn', outputs)
 
     def validation_step(self, batch, batch_idx):
         return self.shared_step('val', batch)
@@ -55,53 +58,61 @@ class Module(pl.LightningModule):
     def validation_epoch_end(self, outputs):
         self.shared_epoch_end('val', outputs)
 
-    def test_step(self, batch, batch_idx):
-        return self.shared_step('test', batch)
+    @property
+    def num_devices(self) -> int:
+        t = self.trainer
+        return t.num_nodes * max(t.num_processes, t.num_gpus, t.tpu_cores or 0)
 
-    def test_epoch_end(self, outputs):
-        self.shared_epoch_end('test', outputs)
+    @property
+    def steps_per_epoch(self) -> int:
+        num_accum = self.trainer.accumulate_grad_batches
+        return len(self.train_dataloader()) // (num_accum * self.num_devices)
 
     def configure_optimizers(self):
         optim = get_optim(self, **self.hparams.optimizer)
-        sched = get_sched(optim, **self.hparams.scheduler)
-        return {
-            'optimizer': optim,
-            'lr_scheduler': {
-                'name': "lr",
-                'scheduler': sched,
-                **self.hparams.lr_dict
-            },
-        }
+        sched = get_lr_dict(optim, steps_per_epoch=self.steps_per_epoch, **self.hparams.lr_dict)
+        return {'optimizer': optim, 'lr_scheduler': sched}
 
 
-def run(hparams):
-    transform_train = get_trfms(hparams['transform']['train'])
-    transform_valid = get_trfms(hparams['transform']['valid'])
-    dataset_train = datasets.CIFAR10(
-        './data', train=True, transform=transform_train, download=True)
-    dataset_valid = datasets.CIFAR10(
-        './data', train=False, transform=transform_valid, download=True)
-    dataloader_train = torch.utils.data.DataLoader(
-        dataset_train, batch_size=hparams['dataset']['batch_size'],
-        shuffle=True, num_workers=os.cpu_count(), pin_memory=True)
-    dataloader_valid = torch.utils.data.DataLoader(
-        dataset_valid, batch_size=hparams['dataset']['batch_size'],
-        shuffle=False, num_workers=os.cpu_count(), pin_memory=True)
+def run(hparams, args):
+    callbacks = [ModelCheckpoint(save_top_k=0), LearningRateMonitor()]
+    trainer = pl.Trainer.from_argparse_args(args, callbacks=callbacks)
 
-    pl_module = Module(**hparams)
+    num_devices = trainer.num_nodes * max(
+        trainer.num_processes, trainer.num_gpus, trainer.tpu_cores or 0)
 
-    trainer = pl.Trainer(
-        callbacks=[
-            ModelCheckpoint(save_top_k=1, monitor='val/acc'),
-            LearningRateMonitor(logging_interval=hparams['lr_dict']['interval']),
-        ],
-        **hparams['trainer'])
+    Dataset = datasets[hparams['dataset']['name']]
+    root = os.path.join('data', hparams['dataset']['name'])
+    batch_size = hparams['dataset']['batch_size'] // num_devices
 
-    trainer.fit(pl_module, dataloader_train, dataloader_valid)
-    trainer.test(test_dataloaders=dataloader_valid)
+    transform = {
+        'train': get_trfms(hparams['transform']['train']),
+        'valid': get_trfms(hparams['transform']['valid']),
+    }
+    dataset = {
+        'train': Dataset(root, train=True, transform=transform['train']),
+        'valid': Dataset(root, train=False, transform=transform['valid']),
+    }
+    dataloader = {
+        'train': torch.utils.data.DataLoader(
+            dataset['train'], batch_size, shuffle=True,
+            num_workers=os.cpu_count(), pin_memory=True),
+        'valid': torch.utils.data.DataLoader(
+            dataset['valid'], batch_size, shuffle=False,
+            num_workers=os.cpu_count(), pin_memory=True),
+    }
+
+    model = Module(**hparams)
+    trainer.fit(model, dataloader['train'], dataloader['valid'])
 
 
 if __name__ == "__main__":
-    with open(sys.argv[1]) as file:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('config', type=str)
+    parser = pl.Trainer.add_argparse_args(parser)
+    args = parser.parse_args()
+
+    with open(args.config) as file:
         hparams = json.load(file)
-    run(hparams)
+
+    run(hparams, args)
