d32aeb7b33af079d2c8852842406d80ab33c624f
Holim Lim <holim0711@outlook.com>
2021-10-03 15:09:59 +0900

    simple dataset

--- weaver/datasets.py
+++ weaver/datasets.py
@@ -1,72 +1,72 @@
 import os
-import random
-from PIL import Image
+from collections.abc import Iterable
+from typing import Union, Callable
+from torch.utils.data import Dataset, DataLoader
 import torch
 
 __all__ = [
-    'Dataset',
-    'RandomSubset',
     'SimpleDataset',
+    'get_dataloader',
 ]
 
 
-class Dataset(torch.utils.data.Dataset):
-    def get_dataloader(
-        self,
-        batch_size,
-        shuffle=False,
-        num_workers=os.cpu_count(),
-        pin_memory=True,
+def get_dataloader(
+    dataset: Dataset,
+    batch_size: int,
+    shuffle: bool,
+    num_workers: int = os.cpu_count(),
+    pin_memory: bool = torch.cuda.is_available(),
+    **kwargs
+):
+    return DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        num_workers=num_workers,
+        pin_memory=pin_memory,
         **kwargs
-    ):
-        return torch.utils.data.DataLoader(
-            self,
-            batch_size=batch_size,
-            shuffle=shuffle,
-            num_workers=num_workers,
-            pin_memory=pin_memory,
-            **kwargs)
-
-
-class RandomSubset(Dataset):
-    def __init__(self, dataset, n):
-        self.dataset = dataset
-        self.indices = random.sample(range(len(self.dataset)), n)
-
-    def __len__(self):
-        return len(self.indices)
-
-    def __getitem__(self, idx):
-        return self.dataset[self.indices[idx]]
+    )
 
 
 class SimpleDataset(Dataset):
-    def __init__(self, data, reader=None, transform=None, target_transform=None):
-        """ Simple Image Dataset
+    def __init__(
+        self,
+        data: Union[Iterable, tuple[Iterable, Iterable]],
+        transform_x: Callable = None,
+        transform_y: Callable = None
+    ):
+        """ Simple Dataset
 
         Args
-            - data (list): `x, y = data[i]`
-            - reader (func): `x = reader(root, x)`
-            - transform (func): `x = transform(x)`
-            - target_transform (func): `y = target_transform(y)`
+            - data: `x, y = data[i]` or `x, y = data[0][idx], data[1][idx]`
+            - transform_x: `x = transform_x(x)`
+            - transform_y: `y = transform_y(y)`
         """
         self.data = data
-        self.reader = reader
-        self.transform = transform
-        self.target_transform = target_transform
+        self.transform_x = transform_x
+        self.transform_y = transform_y
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        x, y = self.data[idx]
+        if isinstance(self.data, tuple):
+            x, y = self.data[0][idx], self.data[1][idx]
+        else:
+            x, y = self.data[idx]
 
-        x = x if self.reader is None else self.reader(x)
+        if self.transform_x:
+            x = self.transform_x(x)
 
-        if self.transform is not None:
-            x = self.transform(x)
-
-        if self.target_transform is not None:
-            y = self.target_transform(y)
+        if self.transform_y:
+            y = self.transform_y(y)
 
         return x, y
+
+    def get_dataloader(
+        self,
+        batch_size: int,
+        shuffle: bool,
+        **kwargs
+    ):
+        return get_dataloader(self, batch_size, shuffle, **kwargs)
