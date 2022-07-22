26f017cf2fbf264f0cc33dc3c805cbb146339dd2
Holim Lim <holim0711@outlook.com>
2022-07-23 01:00:27 +0900

    models

--- src/weaver/models/__init__.py
+++ src/weaver/models/__init__.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 import torchvision
 from .custom import get_custom_classifier
 
-__all__ = ['get_classifier', 'get_vectorizer']
+__all__ = ['get_classifier', 'get_featurizer', 'FEATURE_DIMENSION']
 
 
 def get_classifier(src: str, name: str, **kwargs):
@@ -14,21 +14,54 @@ def get_classifier(src: str, name: str, **kwargs):
     raise ValueError(f"Unknown source: {src}")
 
 
-def get_vectorizer(src: str, name: str, **kwargs):
+def get_featurizer(src: str, name: str, **kwargs):
     model = get_classifier(src, name, **kwargs)
 
-    candidates = ['fc', '_fc', 'classifier']
+    candidates = ['fc', 'classifier']
 
     for c in candidates:
         if hasattr(model, c):
             fc = getattr(model, c)
             if isinstance(fc, nn.Linear):
                 setattr(model, c, nn.Identity())
-                return model  # resnet style
+                return model  # resnet, ...
             elif isinstance(fc, nn.Sequential):
                 if isinstance(fc[-1], nn.Linear):
                     fc[-1] = nn.Identity()
-                    return model  # efficientnet style
+                    return model  # vgg, efficientnet, ...
             raise NotImplementedError(type(fc))
 
     raise ValueError(f"Cannot find {candidates}")
+
+
+FEATURE_DIMENSION = {
+    'weaver': {
+        'wide_resnet28_2': 128,
+        'wide_resnet28_10': 640,
+        'preact_resnet18': 512,
+    },
+    'torchvision': {
+        'vgg11': 4096,
+        'vgg13': 4096,
+        'vgg16': 4096,
+        'vgg19': 4096,
+        'vgg11_bn': 4096,
+        'vgg13_bn': 4096,
+        'vgg16_bn': 4096,
+        'vgg19_bn': 4096,
+        'resnet18': 512,
+        'resnet34': 512,
+        'resnet50': 2048,
+        'resnet101': 2048,
+        'resnet152': 2048,
+        'efficientnet_b0': 1280,
+        'efficientnet_b1': 1280,
+        'efficientnet_b1': 1280,
+        'efficientnet_b2': 1408,
+        'efficientnet_b3': 1536,
+        'efficientnet_b4': 1792,
+        'efficientnet_b5': 2048,
+        'efficientnet_b6': 2304,
+        'efficientnet_b7': 2560,
+    },
+}
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,36 +1,53 @@
 import unittest
 import torch
 from torch.utils.tensorboard import SummaryWriter
-from weaver.models import get_classifier, get_vectorizer
+from weaver.models import get_classifier, get_featurizer, FEATURE_DIMENSION
 
 
 class TestModels(unittest.TestCase):
 
-    def visualize_models(self, src, name, input_shape, **kwargs):
-        writer = SummaryWriter(f'runs/{src}/{name}')
+    def template_classifier_tests(self, src, names, input_shape, **kwargs):
+        for name in names:
+            m = get_classifier(src, name, **kwargs)
+            with SummaryWriter(f'runs/{src}/{name}') as writer:
+                writer.add_graph(m, torch.randn(input_shape))
 
-        cls = get_classifier(src, name, **kwargs)
-        vec = get_vectorizer(src, name, **kwargs)
+    def template_featurizer_tests(self, src, names, input_shape, **kwargs):
+        for name in names:
+            m = get_featurizer(src, name, **kwargs)
+            m.eval()
+            with torch.no_grad():
+                v = m(torch.randn(input_shape))
+            self.assertEqual(v.shape[0], input_shape[0])
+            self.assertEqual(v.shape[1], FEATURE_DIMENSION[src][name])
 
-        x = torch.randn(input_shape)
+    def test_torchvision_classifiers(self):
+        self.template_classifier_tests(
+            'torchvision',
+            ['resnet18', 'efficientnet_b0'],
+            (1, 3, 224, 224)
+        )
 
-        writer.add_graph(cls, x)
-        writer.add_graph(vec, x)
-        writer.close()
+    def test_weaver_classifiers(self):
+        self.template_classifier_tests(
+            'weaver',
+            ['wide_resnet28_2', 'preact_resnet18'],
+            (1, 3, 32, 32)
+        )
 
-    def test_torchvision_models(self):
-        src = 'torchvision'
-        models = ['resnet18', 'efficientnet_b0']
-        input_shape = (1, 3, 224, 224)
-        for model in models:
-            self.visualize_models(src, model, input_shape)
+    def test_torchvision_featurizers(self):
+        self.template_featurizer_tests(
+            'torchvision',
+            ['resnet18', 'efficientnet_b0'],
+            (1, 3, 224, 224)
+        )
 
-    def test_weaver_models(self):
-        src = 'weaver'
-        models = ['wide_resnet28_2', 'preact_resnet18']
-        input_shape = (1, 3, 32, 32)
-        for model in models:
-            self.visualize_models(src, model, input_shape)
+    def test_weaver_featurizers(self):
+        self.template_featurizer_tests(
+            'weaver',
+            ['wide_resnet28_2', 'preact_resnet18'],
+            (1, 3, 32, 32)
+        )
 
 
 if __name__ == "__main__":
