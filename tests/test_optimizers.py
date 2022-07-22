568c43dd85e14f76e42356dae9c1ce92d3f06e93
Holim Lim <holim0711@outlook.com>
2022-07-23 01:05:32 +0900

    test optimizers

--- tests/test_optimizers.py
+++ tests/test_optimizers.py
@@ -1,16 +1,19 @@
-from holim_lightning.models import get_model
-from holim_lightning.optimizers import get_optim
+import unittest
+from weaver.models import get_classifier
+from weaver.optimizers import get_optim
 
 
-def test_optimizers():
-    model = get_model('torchvision', 'resnet18', pretrained=True)
-    optim = get_optim(model.parameters(), 'SGD', lr=0.3)
-    print(optim)
-    optim = get_optim(model.parameters(), 'AdaBound', lr=1e-3, final_lr=0.1)
-    print(optim)
-    optim = get_optim(model.parameters(), 'LARS', lr=0.1, momentum=0.9, nesterov=True)
-    print(optim)
+class TestModels(unittest.TestCase):
+
+    def test_optimizers(self):
+        model = get_classifier('torchvision', 'resnet18')
+
+        optim = get_optim(model, 'SGD', lr=0.1)
+        self.assertEqual(optim.__class__.__name__, 'SGD')
+
+        optim = get_optim(model.parameters(), 'LARS', lr=0.1, momentum=0.9)
+        self.assertEqual(optim.__class__.__name__, 'LARS')
 
 
 if __name__ == "__main__":
-    test_optimizers()
+    unittest.main()
