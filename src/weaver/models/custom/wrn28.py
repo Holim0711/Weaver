0cff02923d8186651d76ebf0c97301a5584cdddd
Holim Lim <holim0711@outlook.com>
2022-07-29 17:33:33 +0000

    remove leaky ReLU

--- src/weaver/models/custom/wrn28.py
+++ src/weaver/models/custom/wrn28.py
@@ -2,10 +2,7 @@ import torch.nn as nn
 
 
 class BasicBlock(nn.Module):
-    def __init__(
-        self, cᵢ: int, cₒ: int, s: int, leaky_slope: float = 0.,
-        activate_before_residual: bool = False
-    ):
+    def __init__(self, cᵢ: int, cₒ: int, s: int, activate_before_residual: bool = False):
         super().__init__()
 
         # Why using `activate_before_residual`
@@ -17,8 +14,8 @@ class BasicBlock(nn.Module):
         self.conv2 = nn.Conv2d(cₒ, cₒ, 3, 1, 1, bias=False)
         self.bn1 = nn.BatchNorm2d(cᵢ)
         self.bn2 = nn.BatchNorm2d(cₒ)
-        self.relu1 = nn.LeakyReLU(leaky_slope, inplace=True)
-        self.relu2 = nn.LeakyReLU(leaky_slope, inplace=True)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.relu2 = nn.ReLU(inplace=True)
 
         # shortcut path
         if (cᵢ == cₒ) and (s == 1):
@@ -38,13 +35,7 @@ class BasicBlock(nn.Module):
 
 
 class WideResNet(nn.Module):
-    def __init__(
-        self,
-        depth: int = 28,
-        width: int = 2,
-        num_classes: int = 10,
-        leaky_slope: float = 0.,
-    ):
+    def __init__(self, depth: int, width: int, num_classes: int = 10):
         super().__init__()
 
         assert (depth - 4) % 6 == 0
@@ -52,30 +43,28 @@ class WideResNet(nn.Module):
         c = [16, 16 * width, 32 * width, 64 * width]
 
         self.conv0 = nn.Conv2d(3, c[0], 3, 1, 1, bias=False)
-        self.block1 = self._make_layer(n, c[0], c[1], 1, leaky_slope,
-                                       activate_before_residual=True)
-        self.block2 = self._make_layer(n, c[1], c[2], 2, leaky_slope)
-        self.block3 = self._make_layer(n, c[2], c[3], 2, leaky_slope)
+        self.block1 = self._make_layer(n, c[0], c[1], 1, activate_before_residual=True)
+        self.block2 = self._make_layer(n, c[1], c[2], 2)
+        self.block3 = self._make_layer(n, c[2], c[3], 2)
 
         self.bn = nn.BatchNorm2d(c[-1])
-        self.relu = nn.LeakyReLU(leaky_slope, inplace=True)
+        self.relu = nn.ReLU(inplace=True)
         self.pool = nn.AdaptiveAvgPool2d(1)
         self.flatten = nn.Flatten()
         self.fc = nn.Linear(c[-1], num_classes)
 
-        self.init_params(leaky_slope)
+        self.init_params()
 
-    def _make_layer(self, n, cᵢ, cₒ, s, leaky_slope,
-                    activate_before_residual=False):
+    def _make_layer(self, n, cᵢ, cₒ, s, activate_before_residual=False):
         return nn.Sequential(*[
-            BasicBlock(cᵢ, cₒ, s, leaky_slope, activate_before_residual),
-            *[BasicBlock(cₒ, cₒ, 1, leaky_slope) for _ in range(1, n)]
+            BasicBlock(cᵢ, cₒ, s, activate_before_residual),
+            *[BasicBlock(cₒ, cₒ, 1) for _ in range(1, n)]
         ])
 
-    def init_params(self, leaky_slope):
+    def init_params(self, a=0.0):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, leaky_slope, mode='fan_out')
+                nn.init.kaiming_normal_(m.weight, a, mode='fan_out')
             elif isinstance(m, nn.BatchNorm2d):
                 nn.init.constant_(m.weight, 1.0)
                 nn.init.constant_(m.bias, 0.0)
