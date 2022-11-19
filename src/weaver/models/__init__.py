5959b21e744da51b9f3f23b7459bba852b0fdf96
Holim Lim <holim0711@outlook.com>
2022-11-19 15:26:00 +0000

    wow

--- src/weaver/models/__init__.py
+++ src/weaver/models/__init__.py
@@ -7,10 +7,12 @@ __all__ = ['get_classifier']
 def get_classifier(src: str, name: str, **kwargs):
     src = src.lower()
     if src == 'weaver':
-        if name == 'PreactResNet':
-            return PreactResNet(**kwargs)
-        if name == 'WideResNet':
-            return WideResNet(**kwargs)
+        if name.startswith('preact_resnet'):
+            depth = int(name[13:])
+            return PreactResNet(depth, **kwargs)
+        if name.startswith('wide_resnet'):
+            depth, width = map(int, name[11:].split('_'))
+            return WideResNet(depth, width, **kwargs)
         raise ValueError(f"Unknown model name: {name}")
     if src == 'torchvision':
         return get_model(name, **kwargs)
