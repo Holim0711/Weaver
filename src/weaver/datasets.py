d2b716ed04bccfa199ece2fe81c89d2837f8235c
Holim Lim <holim0711@outlook.com>
2023-10-21 23:14:07 +0900

    split random-fewshot

--- src/weaver/datasets.py
+++ src/weaver/datasets.py
@@ -17,34 +17,38 @@ class IndexedDataset(Dataset):
         return index, self.dataset[index]
 
 
-def balanced_random_select(targets, length, random_state):
-    targets = np.array(targets)
-    classes = np.unique(targets)
-    n = length // len(classes)
-
-    results = []
-    for c in classes:
-        indices = np.where(targets == c)[0]
-        indices = random_state.choice(indices, n, replace=False)
-        results.extend(indices)
-    return results
-
-
 class RandomSubset(Subset):
     def __init__(
         self,
         dataset: Dataset,
         length: Union[int, float],
-        class_balanced: bool = False,
         random_seed: int = 0,
     ):
         if length < 1.0:
             length = round(len(dataset) * length)
+        self.length = length
+        self.random_state = np.random.RandomState(random_seed)
+        indices = self.random_state.choice(len(dataset), length, replace=False)
+        super().__init__(dataset, indices)
 
-        random_state = np.random.RandomState(random_seed)
-        if not class_balanced:
-            indices = random_state.choice(len(dataset), length, replace=False)
+
+class FewShotSubset(Subset):
+    def __init__(
+        self,
+        dataset: Dataset,
+        k_shots: int,
+        random_seed: int = 0,
+    ):
+        if hasattr(dataset, 'targets'):
+            targets = np.array(dataset.targets)
         else:
-            targets = [int(y) for _, y in dataset]
-            indices = balanced_random_select(targets, length, random_state)
+            targets = np.array([y for x, y in dataset])
+        self.n_ways = targets.max() + 1
+        self.k_shots = k_shots
+        self.random_state = np.random.RandomState(random_seed)
+        indices = []
+        for c in range(self.n_ways):
+            c_idxs = np.where(targets == c)[0]
+            c_idxs = self.random_state.choice(c_idxs, k_shots, replace=False)
+            indices.extend(c_idxs)
         super().__init__(dataset, indices)
